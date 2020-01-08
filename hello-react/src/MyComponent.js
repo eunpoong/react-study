7a8c9d57f64273ac0f946f5c025c6b0c4bb93502
junyeong <junyeong@cometwiz.com>
2020-01-08 12:44:30 +0900

    ch.3.3.5

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -4,18 +4,33 @@
  * 3.3.2 컴포넌트 사용할 떄 props 값 지정
  * 3.3.3. props 기본값 설정 : defaultProps
  * 3.3.4 children
+ * 3.3.5 비구조화
  */
 
 import React from 'react';
 
+/*
 const MyComponent = (props) => {
+  const { name, children } = props; //비구조화 할당
+
   return (
     <div>
-      <div>이름 : {props.name}</div>
-      <div>children : {props.children}</div>
+      <div>이름 : {name}</div>
+      <div>children : {children}</div>
     </div>
   );
 }
+*/
+
+const MyComponent = ({ name, children }) => {
+
+  return (
+    <div>
+      <div>이름 : {name}</div>
+      <div>children : {children}</div>
+    </div>
+  );
+};
 
 MyComponent.defaultProps = {
   name : '기본값'
