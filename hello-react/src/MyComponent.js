545e24bdf898d98412e56995dd1ca008ce719513
junyeong <junyeong@cometwiz.com>
2020-01-08 12:41:23 +0900

    ch.3.3.2

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,7 +1,17 @@
+/**
+ * 3.3 props
+ * 3.3.1 jsx 내부 props
+ * 3.3.2 컴포넌트 사용할 떄 props 값 지정
+ */
+
 import React from 'react';
 
-const MyComponent = () => {
-  return <div>컴포넌트</div>;
+const MyComponent = (props) => {
+  return (
+    <div>
+      <div>이름 : {props.name}</div>
+    </div>
+  );
 }
 
 export default MyComponent;
