7765df5f3f7390d3c0702ccc9effbfeecc4cb73b
junyeong <junyeong@cometwiz.com>
2020-01-08 15:19:48 +0900

    ch.4.2.2.2

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,11 +1,16 @@
 /**
  * 4.2 예제로 이벤트 핸들링
  * 4.2.2.1 onChange 이벤트 핸들링
+ * 4.2.2.2 state 에 input 값 담기
  */
 
 import React, { Component } from 'react';
 
 class EventPractice extends Component {
+  state = {
+    message: ''
+  };
+
   render() {
     return (
       <div>
@@ -16,6 +21,9 @@ class EventPractice extends Component {
           placeholder="아무거나 입력"
           onChange={e => {
             console.log(e.target.value);
+            this.setState({
+              message: e.target.value
+            });
           }}
         />
       </div>
