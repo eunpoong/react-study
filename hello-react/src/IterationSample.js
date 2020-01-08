d5eb670c8ea1069ea656ef0b5bf28560a8f49a1b
junyeong <junyeong@cometwiz.com>
2020-01-08 20:31:41 +0900

    ch.6.4.1

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -1,13 +1,26 @@
 /**
  * 6.2 데이터 배열을 컴포넌트 배열로 변환하기
  * 6.3 key
+ * 6.4 응용
+ * 6.4.1 초기상태 설정
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 
 const IterationSample = () => {
+  /*
   const names = ['가', '나', '다', '라'];
   const nameList = names.map((name, index) => <li key={index}>{name}</li>);
+  */
+  const [names, setNames] = useState([
+    { id: 1, text: '가' },
+    { id: 2, text: '나' },
+    { id: 3, text: '다' },
+    { id: 4, text: '라' }
+  ]);
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(5);
+  const nameList = names.map(name => <li key={name.id}>{name.text}</li>);
   return <ul>{nameList}</ul>;
 };
 
