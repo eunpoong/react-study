ec6f87b99833301c38867aaf8037edca9df2e0ee
junyeong <junyeong@cometwiz.com>
2020-01-08 14:57:04 +0900

    ch.3.4.1.3

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -3,6 +3,7 @@
  * 3.4.1 클래스형 컴포넌트의 state
  * 3.4.1.1 state 여라 값
  * 3.4.1.2 state 를 constructor에서 꺼내기
+ * 3.4.1.3 this.setState에 객체 대신 함수 인자 전달하기
  */
 
 import React, { Coponent, Component } from "react";
@@ -21,8 +22,18 @@ class Counter extends Component {
         <h2>바뀌지 않는 값 : {fixedNumber}</h2>
 
         <button
+          // onClick을 통해 버튼이 클릭되었을 떄 호출할 함수를 지정
           onClick={() => {
-            this.setState({ number: number + 1 });
+            this.setState(prevState => {
+              return {
+                number: prevState.number + 1
+              };
+            });
+            // 위 코드 아래코드 똑같은 기능
+            // 아래 코드는 함수에서 바로 객체를 반환한다는 의미
+            this.setState(prevState => ({
+              number: prevState.number + 1
+            }));
           }}
         >
           +1
