1340b9f9c859785fea7795cee088021c6af791a8
junyeong <junyeong@cometwiz.com>
2020-01-08 12:42:37 +0900

    ch.3.3.4

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -3,6 +3,7 @@
  * 3.3.1 jsx 내부 props
  * 3.3.2 컴포넌트 사용할 떄 props 값 지정
  * 3.3.3. props 기본값 설정 : defaultProps
+ * 3.3.4 children
  */
 
 import React from 'react';
@@ -11,6 +12,7 @@ const MyComponent = (props) => {
   return (
     <div>
       <div>이름 : {props.name}</div>
+      <div>children : {props.children}</div>
     </div>
   );
 }
