180d4b528600fa5a1670429a76cc1a741eaf6fb9
junyeong <junyeong@cometwiz.com>
2020-01-08 20:28:08 +0900

    ch.6.3

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -1,12 +1,13 @@
 /**
  * 6.2 데이터 배열을 컴포넌트 배열로 변환하기
+ * 6.3 key
  */
 
 import React from 'react';
 
 const IterationSample = () => {
   const names = ['가', '나', '다', '라'];
-  const nameList = names.map(name => <li>{name}</li>);
+  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
   return <ul>{nameList}</ul>;
 };
 
