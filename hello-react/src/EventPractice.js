f313b01b98ebdad20f454ca90ecd3a0fc14f3c70
junyeong <junyeong@cometwiz.com>
2020-01-08 15:22:29 +0900

    ch.4.2.2.3

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -2,6 +2,7 @@
  * 4.2 예제로 이벤트 핸들링
  * 4.2.2.1 onChange 이벤트 핸들링
  * 4.2.2.2 state 에 input 값 담기
+ * 4.2.2.3 버튼 누를때 comment 값 공백으로 설정
  */
 
 import React, { Component } from 'react';
@@ -26,6 +27,17 @@ class EventPractice extends Component {
             });
           }}
         />
+        <button
+          onClick={() => {
+            console.log('before', this.state.message);
+            this.setState({
+              message: ''
+            });
+            console.log('after', this.state.message);
+          }}
+        >
+          확인
+        </button>
       </div>
     );
   }
