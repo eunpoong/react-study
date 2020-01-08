208e055721a3406cc69b8ce8a799370e8b83e64b
junyeong <junyeong@cometwiz.com>
2020-01-08 12:41:39 +0900

    ch.3.3.3

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -2,6 +2,7 @@
  * 3.3 props
  * 3.3.1 jsx 내부 props
  * 3.3.2 컴포넌트 사용할 떄 props 값 지정
+ * 3.3.3. props 기본값 설정 : defaultProps
  */
 
 import React from 'react';
@@ -12,6 +13,10 @@ const MyComponent = (props) => {
       <div>이름 : {props.name}</div>
     </div>
   );
+}
+
+MyComponent.defaultProps = {
+  name : '기본값'
 }
 
 export default MyComponent;
