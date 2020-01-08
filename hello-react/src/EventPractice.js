6a313142f7e869feaa9d64b182916193585ece77
junyeong <junyeong@cometwiz.com>
2020-01-08 15:25:35 +0900

    ch.4.2.3.1

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -3,6 +3,10 @@
  * 4.2.2.1 onChange 이벤트 핸들링
  * 4.2.2.2 state 에 input 값 담기
  * 4.2.2.3 버튼 누를때 comment 값 공백으로 설정
+ *
+ * 4.2.3 임의 메서드 만들기
+ * 4.2.3.1 기본방식
+ *
  */
 
 import React, { Component } from 'react';
@@ -12,6 +16,22 @@ class EventPractice extends Component {
     message: ''
   };
 
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      message: e.target.value
+    });
+  }
+  handleClick() {
+    console.log('click', this.state.message);
+    this.setState({
+      message: ''
+    });
+  }
   render() {
     return (
       <div>
@@ -20,24 +40,9 @@ class EventPractice extends Component {
           type="text"
           name="message"
           placeholder="아무거나 입력"
-          onChange={e => {
-            console.log(e.target.value);
-            this.setState({
-              message: e.target.value
-            });
-          }}
+          onChange={this.handleChange}
         />
-        <button
-          onClick={() => {
-            console.log('before', this.state.message);
-            this.setState({
-              message: ''
-            });
-            console.log('after', this.state.message);
-          }}
-        >
-          확인
-        </button>
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
