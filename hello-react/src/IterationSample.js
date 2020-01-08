6bd52bd48726611b80245d81e51e308d39e41083
junyeong <junyeong@cometwiz.com>
2020-01-08 20:34:40 +0900

    ch.6.4.2

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -3,6 +3,7 @@
  * 6.3 key
  * 6.4 응용
  * 6.4.1 초기상태 설정
+ * 6.4.2 데이터 추가 기능
  */
 
 import React, { useState } from 'react';
@@ -21,7 +22,23 @@ const IterationSample = () => {
   const [inputText, setInputText] = useState('');
   const [nextId, setNextId] = useState(5);
   const nameList = names.map(name => <li key={name.id}>{name.text}</li>);
-  return <ul>{nameList}</ul>;
+  const onChange = e => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText
+    });
+    setNextId(nextId + 1);
+    setNames(nextNames);
+    setInputText('');
+  };
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{nameList}</ul>
+    </>
+  );
 };
 
 export default IterationSample;
