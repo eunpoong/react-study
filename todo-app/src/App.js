99a56111b15b1cb0383fef8cfe40ca192cdc34b8
junyeong <junyeong@cometwiz.com>
2020-01-09 13:47:57 +0900

    ch.11.4 React.memo 사용

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,6 +1,11 @@
 /**
  * 10 Todo App
  * 10.2 UI 구성
+ *
+ * 11. 컴포넌트 성능 최적화
+ * 11.1 많은 데이터 렌더링하기
+ * 11.4 React.memo를 사용하여 컴포넌트 성능 최적화
+ *
  */
 
 import React, { useState, useRef, useCallback } from 'react';
@@ -8,14 +13,29 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
 const App = () => {
+  /*
   const [todos, setTodos] = useState([
     { id: 1, text: '리택트 기초 알아보기', checked: true },
     { id: 2, text: '컴포넌트 스타일링해 보기', checked: true },
     { id: 3, text: '일정 관리 앱 만들어 보기', checked: false },
   ]);
+  */
+  const [todos, setTodos] = useState(createBulkTodos);
 
-  const nextId = useRef(4);
+  const nextId = useRef(2501);
 
   const onInsert = useCallback(
     text => {
