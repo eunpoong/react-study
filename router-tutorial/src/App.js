e71b0013fcfc71774fdc1fcc05f7fc7e13591556
junyeong <junyeong@cometwiz.com>
2020-01-10 10:20:24 +0900

    ch.13.2.5 Link 컴포넌트 사용, 주소 이동

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,15 +1,25 @@
 /**
  * 13.2.2 프로젝트에 라우터 적용
  * 13.2.4 Route 컴포넌트로 특정 주소에 컴포넌트 연결
+ * 13.2.5 Link 컴포넌트 사용, 주소 이동
  */
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 
 const App = () => {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">Hone</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <hr />
       <Route exact={true} path="/" component={Home} />
       <Route exact={true} path="/about" component={About} />
     </div>
