f24bfe9e2d86ea8c404349afaf3630dc5a515132
junyeong <junyeong@cometwiz.com>
2020-01-10 18:33:50 +0900

    ch.14.2 axios로 API 호출해서 데이터 받아오기

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import AsyncTest from './AsyncTest';
+/**
+ * 14.2 axios로 API 호출해서 데이터 받아오기
+ */
 
-function App() {
-  return <AsyncTest />;
-}
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = () => {
+    axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+      setData(response.data);
+    });
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
+};
 
 export default App;
