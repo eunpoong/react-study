9d8b701861bca85f7a2bf58a70c4b809dc756ea6
junyeong <junyeong@cometwiz.com>
2020-01-10 10:22:01 +0900

    ch.13.3 Route 하나에 여러개의 path 설정하기

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,6 +2,7 @@
  * 13.2.2 프로젝트에 라우터 적용
  * 13.2.4 Route 컴포넌트로 특정 주소에 컴포넌트 연결
  * 13.2.5 Link 컴포넌트 사용, 주소 이동
+ * 13.3 Route 하나에 여러개의 path 설정하기
  */
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
@@ -21,7 +22,11 @@ const App = () => {
       </ul>
       <hr />
       <Route exact={true} path="/" component={Home} />
+      {/*
       <Route exact={true} path="/about" component={About} />
+      <Route exact={true} path="/info" component={About} />
+      */}
+      <Route exact={true} path={['/about', '/info']} component={About} />
     </div>
   );
 };
