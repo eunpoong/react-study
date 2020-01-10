5d801912431f426fcca105d335dd9e1f81cf9c35
junyeong <junyeong@cometwiz.com>
2020-01-10 20:49:40 +0900

    ch.14.7 리액트 라우터 적용하기

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -4,21 +4,16 @@
  * 14.5 데이터 연동하기
  * 14.6 카테고리 기능 구현
  * 14.6.2 API 호출할 때 카테고리 지정하기
+ * 14.7 리액트 라우터 적용하기
  */
 
-import React, { useState, useCallback } from 'react';
-import NewsList from './components/NewsList';
-import Categories from './components/Categories';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback(category => setCategory(category), []);
-  return (
-    <>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-    </>
-  );
+  /* path에 물음표 문자, category 값이 optional 이라는 의미 */
+  return <Route path="/:category?" component={NewsPage} />;
 };
 
 export default App;
