49ac65d8fd89423120d50ee79a1ac1c6ac05a59b
junyeong <junyeong@cometwiz.com>
2020-01-12 17:32:33 +0900

    ch.17.2.2 할 일 목록 컴포넌트

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -1,14 +1,18 @@
 /**
  * 17.2.1 카운터 컴포넌트
+ * 17.2.2 할 일 목록 컴포넌트
  */
 
 import React from 'react';
 import Counter from './components/Counter';
+import Todos from './components/Todos';
 
 const App = () => {
   return (
     <div>
       <Counter number={0} />
+      <hr />
+      <Todos />
     </div>
   );
 };
