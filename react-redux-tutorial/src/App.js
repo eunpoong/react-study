e97104679d5ebba578f928ddf5929c41ce92d155
junyeong <junyeong@cometwiz.com>
2020-01-12 17:27:41 +0900

    ch.17.2.1 카운터 컴포넌트

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/**
+ * 17.2.1 카운터 컴포넌트
+ */
 
-function App() {
+import React from 'react';
+import Counter from './components/Counter';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Counter number={0} />
     </div>
   );
-}
+};
 
 export default App;
