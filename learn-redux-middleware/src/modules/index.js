a51eea460e9954412d68105a382640e82d880f4d
junyeong <junyeong@cometwiz.com>
2020-01-12 22:57:47 +0900

    ch.18.3.2.2 Redux-saga - 루트 리듀서

--- learn-redux-middleware/src/modules/index.js
+++ learn-redux-middleware/src/modules/index.js
@@ -1,10 +1,13 @@
 /**
  * 18.1 작업 환경 준비 - 루트 리듀서 작성
  * 18.3.1.4 웹 요청 비동기 작업 - 루트 리듀서 포함
+ *
+ * 18.3.2.2 Redux-saga - 루트 리듀서
  */
 
 import { combineReducers } from 'redux';
-import counter from './counter';
+import { all } from 'redux-saga/effects';
+import counter, { counterSaga } from './counter';
 import sample from './sample';
 import loading from './loading';
 
@@ -13,5 +16,10 @@ const rootReducer = combineReducers({
   sample,
   loading
 });
+
+export function* rootSaga() {
+  // all 함수는 여러 사가를 합쳐주는 역할
+  yield all([counterSaga()]);
+}
 
 export default rootReducer;
