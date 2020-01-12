73f6349621f0b7ef72aa56b7fadc49ac50c8fc5a
junyeong <junyeong@cometwiz.com>
2020-01-12 17:53:34 +0900

    ch.17.4.1 스토어 만들기

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -1,8 +1,16 @@
+/**
+ * 17.4.1 스토어 만들기
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
