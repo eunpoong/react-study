c6511e45346245453e4a4e66dcebb22c73986bc7
junyeong <junyeong@cometwiz.com>
2020-01-12 18:31:58 +0900

    ch.17.5 컨테이너 컴포넌트 만들기

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -4,18 +4,21 @@
  * 17.3 리덕스 관련 코드 - Ducks 패턴
  * 17.3.1 counter 모듈 작성
  * 17.3.2 todos 모듈 만들기
+ * 17.5 컨테이너 컴포넌트 만들기
  */
 
 import React from 'react';
-import Counter from './components/Counter';
-import Todos from './components/Todos';
+//import Counter from './components/Counter';
+//import Todos from './components/Todos';
+import CounterContainer from './containers/CounterContainer';
+import TodosContainer from './containers/TodosContainer';
 
 const App = () => {
   return (
     <div>
-      <Counter number={0} />
+      <CounterContainer />
       <hr />
-      <Todos />
+      <TodosContainer />
     </div>
   );
 };
---
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import Counter from '../components/Counter';
+import { increase, decrease } from '../modules/counter';
+
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
+};
+
+/*
+const mapStateToProps = state => ({
+  number: state.counter.number
+});
+const mapDispatchToProps = dispatch => ({
+  increase: () => {
+    dispatch(increase());
+  },
+  decrease: () => {
+    dispatch(decrease());
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+*/
+
+// connect 함수를 사용할 때는ㅇ ㅣㄹ반적으로 mapStateToProps와 mapDispatchToProps를 미리 선언하고 사용
+// 하지만 connect 함수 내부에 익명 함수 형태로 선언해도 문제가 되지 않음
+/*
+export default connect(
+  state => ({
+    number: state.counter.number
+  }),
+  dispatch => ({
+    increase: () => dispatch(increase()),
+    decrease: () => dispatch(decrease())
+  })
+)(CounterContainer);
+*/
+
+// 컴포넌트에서 액션을 디스패치 하기 위해 각 액션 생성 함수를 호출하고 dispatch로 감싸는 작업을 줄이기 위해
+// 리덕스에서 제공하는 bindActionCreators 유틸 함수를 사용하면 됨
+/*
+export default connect(
+  state => (
+    { number: state.counter.number }
+  ),
+  dispatch => bindActionCreators(
+    {
+      increase,
+      decrease
+    },
+    dispatch
+  )
+)(CounterContainer);
+*/
+
+// 두번째 파라미터를 아예 객체 형태로 넣어주면 connect 함수가 내부적으로 bindActionCreators 작업을 대신해줌
+export default connect(
+  state => ({
+    number: state.counter.number
+  }),
+  {
+    increase,
+    decrease
+  }
+)(CounterContainer);
