5e941eea69b0b9c7fab0abf47e539e45ffc51b02
junyeong <junyeong@cometwiz.com>
2020-01-12 22:59:44 +0900

    ch.18.3.2.2 Redux-saga - 스토어에 redux-saga 미들웨어 적용

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -2,6 +2,8 @@
  * 18.1 작업 환경 준비 - 스토어 생성 및 Provider로 리덕스 적용
  * 18.2.2 redux-logger 사용하기
  * 18.3.1.2 redux-thunk 미들웨어 적용하기
+ *
+ * 18.3.2.2 Redux-saga - 스토어에 redux-saga 미들웨어 적용
  */
 
 import React from 'react';
@@ -11,14 +13,20 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import rootReducer from './modules';
+import rootReducer, { rootSaga } from './modules';
 //import loggerMiddleware from './lib/loggerMiddleware';
 import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 // 미들웨어는 스토어를 생성하는 과정에서 적용한다
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  applyMiddleware(logger, ReduxThunk, sagaMiddleware)
+);
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
