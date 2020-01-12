feb3ce5e342fb120a1b6d2c4832e917f7ea26a68
junyeong <junyeong@cometwiz.com>
2020-01-12 21:07:28 +0900

    ch.18.1 작업 환경 준비

--- learn-redux-middleware/src/App.js
+++ learn-redux-middleware/src/App.js
@@ -1,26 +1,15 @@
+/**
+ * 18.1 작업 환경 준비
+ */
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CounterContainder from './containers/CounterContainer';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CounterContainder />
     </div>
   );
-}
+};
 
 export default App;
