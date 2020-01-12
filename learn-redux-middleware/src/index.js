547d7b0477e131560b86d2efc88d21a34e709122
junyeong <junyeong@cometwiz.com>
2020-01-12 21:22:20 +0900

    ch.18.3.1.2 redux-thunk 미들웨어 적용하기

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -1,6 +1,7 @@
 /**
  * 18.1 작업 환경 준비 - 스토어 생성 및 Provider로 리덕스 적용
  * 18.2.2 redux-logger 사용하기
+ * 18.3.1.2 redux-thunk 미들웨어 적용하기
  */
 
 import React from 'react';
@@ -13,10 +14,11 @@ import * as serviceWorker from './serviceWorker';
 import rootReducer from './modules';
 //import loggerMiddleware from './lib/loggerMiddleware';
 import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
 // 미들웨어는 스토어를 생성하는 과정에서 적용한다
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
