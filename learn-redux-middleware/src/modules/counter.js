e6b3f3b7906fce9b9450c4bed09a3bceaccaee60
junyeong <junyeong@cometwiz.com>
2020-01-12 20:56:35 +0900

    ch.18.1 작업 환경 준비 - counter 리덕스 모듈 작성

---
+++ learn-redux-middleware/src/modules/counter.js
@@ -0,0 +1,22 @@
+/**
+ * 18.1 작업 환경 준비 - counter 리덕스 모듈 작성
+ */
+import { createAction, handleActions } from 'redux-actions';
+
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
+
+const initialState = 0; // 상태는 꼭 객체일 필요가 없습니다. 숫자도 작동해요
+
+const counter = handleActions(
+  {
+    [INCREASE]: state => state + 1,
+    [DECREASE]: state => state - 1
+  },
+  initialState
+);
+
+export default counter;
