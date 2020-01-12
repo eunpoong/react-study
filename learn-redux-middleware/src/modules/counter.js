ce82124f67945bba4eb961a2e2e9d31d1910341c
junyeong <junyeong@cometwiz.com>
2020-01-12 22:56:24 +0900

    18.3.2.2 Redux-saga - 리덕스 모듈

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -1,8 +1,10 @@
 /**
  * 18.1 작업 환경 준비 - counter 리덕스 모듈 작성
  * 18.3.1.3 Thunk 생성 함수 만들기 - 리덕스 모듈 수정
+ *
+ * 18.3.2.2 Redux-saga - 리덕스 모듈
  */
-import { createAction, handleActions } from 'redux-actions';
+/*import { createAction, handleActions } from 'redux-actions';
 
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
@@ -23,6 +25,50 @@ export const decreaseAsync = () => dispatch => {
 };
 
 const initialState = 0; // 상태는 꼭 객체일 필요가 없습니다. 숫자도 작동해요
+
+const counter = handleActions(
+  {
+    [INCREASE]: state => state + 1,
+    [DECREASE]: state => state - 1
+  },
+  initialState
+);
+
+export default counter;
+*/
+import { createAction, handleActions } from 'redux-actions';
+import { deplay, put, takeEvery, takeLatest } from 'redux-saga/effects';
+
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+const INCREASE_ASYNC = 'counter/INCREASE_ASYNC';
+const DECREASE_ASYNC = 'counter/DECREASE_ASYNC';
+
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
+// 마우스 클릭 이벤트가 payload 안에 들어가지 않도록
+// () => undefined를 두번쨰 파라미터로 넣어 줌
+export const increaseAsync = createAction(INCREASE_ASYNC, () => undefined);
+export const decreaseAsync = createAction(DECREASE_ASYNC, () => undefined);
+
+function* increaseSaga() {
+  yield delay(1000); // 1초 기다림
+  yield put(increase()); // 특정 액션을 디스패치 함
+}
+
+function* decreaseSaga() {
+  yield delay(1000);
+  yield put(decrease());
+}
+
+export function* counterSaga() {
+  // takeEvery는 들어오는 모든 액션에 대해 특정 작업을 처리 해줌
+  yield takeEvery(INCREASE_ASYNC, increaseSaga);
+  // takeLatest는 기존에 진행 중이던 작업이 있다면 취소처리하고 가장 마지막으로 실행된 작업만 수행함
+  yield teateLatest(DECREASE_ASYNC, decreaseSaga);
+}
+
+const initialState = 0;
 
 const counter = handleActions(
   {
