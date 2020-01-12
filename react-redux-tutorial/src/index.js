9cbdc31a83786597b2d0acf40f3212e60ff6b49b
junyeong <junyeong@cometwiz.com>
2020-01-12 17:54:38 +0900

    ch.17.4.2 Provider 컴포넌트 사용해서 프로젝트 리덕스 적용

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -1,10 +1,12 @@
 /**
  * 17.4.1 스토어 만들기
+ * 17.4.2 Provider 컴포넌트 사용해서 프로젝트 리덕스 적용
  */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -12,7 +14,12 @@ import rootReducer from './modules';
 
 const store = createStore(rootReducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
