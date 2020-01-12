c2ddd450d02b7563aada05ae1c060e9fcaff6cd4
junyeong <junyeong@cometwiz.com>
2020-01-12 23:32:33 +0900

    ch18 sample api

--- learn-redux-middleware/src/lib/api.js
+++ learn-redux-middleware/src/lib/api.js
@@ -4,10 +4,8 @@
 
 import axios from 'axios';
 
-export const getPost = id => {
+export const getPost = id =>
   axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
-};
 
-export const getUsers = id => {
+export const getUsers = id =>
   axios.get(`https://jsonplaceholder.typicode.com/users`);
-};
