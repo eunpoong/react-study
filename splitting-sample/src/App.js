cf59b25c03dcdc1b818e370c4996ff02000f3519
junyeong <junyeong@cometwiz.com>
2020-01-13 11:27:29 +0900

    ch.19.2.3 Loadable Components를 통한 코드 스플리팅 - 로딩 중 다른 UI 보여주기

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -3,7 +3,7 @@
  * 19.2 React.lazy 와 Suspense를 통한 컴포넌트 코드 스플리팅
  * 19.2.1 state를 사용한 코드 스플리팅
  * 19.2.2 React.lazy와 Suspense 사용하기
- * 19.2.3 Loadable Components를 통한 코드 스플리팅
+ * 19.2.3 Loadable Components를 통한 코드 스플리팅 - 로딩 중 다른 UI 보여주기
  */
 import React, { useState } from 'react';
 import logo from './logo.svg';
@@ -11,7 +11,9 @@ import './App.css';
 //import notify from './notify';
 import loadable from '@loadable/component';
 
-const SplitMe = loadable(() => import('./SplitMe'));
+const SplitMe = loadable(() => import('./SplitMe'), {
+  fallback: <div>loading...</div>
+});
 
 /*function App() {
   const onClick = () => {
