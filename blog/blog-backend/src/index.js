bdcb0456da2592b802c797c98346ceb6e410047f
junyeong <junyeong@cometwiz.com>
2020-01-13 21:58:46 +0900

    ch.21.4 nodemon 사용하기

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,5 +1,13 @@
 /**
  * 21. 백엔드 프로그래밍
  * 21.2 작업환경준비
+ * 21.3 Koa 기본 사용법
+ * 21.4 nodemon 사용하기
  */
-const hello = 'hello';
+const Koa = require('koa');
+
+const app = new Koa();
+
+app.listen(4000, () => {
+  console.log('Listening to port 4000');
+});
