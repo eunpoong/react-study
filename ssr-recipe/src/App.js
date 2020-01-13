7446efa0453f5b231c67f3751c6eeb9e00ece2ca
junyeong <junyeong@cometwiz.com>
2020-01-13 16:52:37 +0900

    ch.20.4.8 redux-saga를 위한 서버 사이드 렌더링 작업

--- ssr-recipe/src/App.js
+++ ssr-recipe/src/App.js
@@ -12,6 +12,7 @@
  * 20.4.5 스크립트로 스토어 초기 상태 주입하기
  * 20.4.6 redux-saga 코드 준비하기
  * 20.4.7 User, UserContainer 컴포넌트 준비하기
+ * 20.4.8 redux-saga를 위한 서버 사이드 렌더링 작업
  */
 import React from 'react';
 import { Route } from 'react-router-dom';
--- ssr-recipe/src/index.server.js
+++ ssr-recipe/src/index.server.js
@@ -18,8 +18,10 @@ import fs from 'fs';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import rootReducer from './modules';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer, { rootSaga } from './modules';
 import PreloadContext from './lib/PreloadContext';
+import { END } from 'redux-saga';
 
 /*const html = ReactDOMServer.renderToString(
   <div>Hello Server Side rendering!</div>
@@ -72,7 +74,13 @@ const serverRender = async (req, res, next) => {
   // 이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버사이드 렌더링을 해줍니다.
 
   const context = {};
-  const store = createStore(rootReducer, applyMiddleware(thunk));
+  const sagaMiddleware = createSagaMiddleware();
+  const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk, sagaMiddleware)
+  );
+
+  const sagaPromise = sagaMiddleware.run(rootSaga).toPromise();
 
   const preloadContext = {
     done: false,
@@ -89,7 +97,9 @@ const serverRender = async (req, res, next) => {
   );
 
   ReactDOMServer.renderToStaticMarkup(jsx); // renderToStaticMarkup 으로 한번 렌더링합니다.
+  store.dispatch(END);
   try {
+    await sagaPromise; // 기존에 진행 중이던 사가들이 모두 끝날때까지 기다립니다.
     await Promise.all(preloadContext.promises); // 모든 프로미스를 기다립니다.
   } catch (e) {
     return res.status(500);
