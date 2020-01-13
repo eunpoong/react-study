edfd6ec03194eb852057146003260e947f1c672a
junyeong <junyeong@cometwiz.com>
2020-01-13 22:12:13 +0900

    ch.21.5 Koa-router 사용하기

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -3,10 +3,25 @@
  * 21.2 작업환경준비
  * 21.3 Koa 기본 사용법
  * 21.4 nodemon 사용하기
+ * 21.5 Koa-router 사용하기
  */
 const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
+const api = require('./api');
 
 const app = new Koa();
+const router = new Router();
+
+// 라우터 설정
+router.use('/api', api.routes()); // api 라우트 적용
+
+// 라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
   console.log('Listening to port 4000');
