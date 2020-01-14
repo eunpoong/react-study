6a269f78642c99309afcbe8a121e20f37d3068e2
junyeong <junyeong@cometwiz.com>
2020-01-14 16:27:48 +0900

    ch.24.1 작업환경 준비하기

---
+++ blog/blog-frontend/src/index.js
@@ -0,0 +1,26 @@
+/**
+ * 24.1 작업환경 준비하기
+ * */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'),
+);
+
+serviceWorker.unregister();
