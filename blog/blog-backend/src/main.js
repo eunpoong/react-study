9ab9925ee24f74f8e2faf692b7f81f6704a3eef4
junyeong <junyeong@cometwiz.com>
2020-01-14 14:48:11 +0900

    ch.22.10 페이지네이션 구현

--- blog/blog-backend/src/main.js
+++ blog/blog-backend/src/main.js
@@ -9,6 +9,7 @@
  * 22.5 데이터베이스 스키마와 모델
  * 22.7~8 데이터 생성, 조회, 삭제, 수정
  * 22.9 요청 검증
+ * 22.10 페이지네이션 구현
  */
 require('dotenv').config();
 import Koa from 'koa';
@@ -17,6 +18,7 @@ import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 
 import api from './api';
+import createFakeData from './createFakeData';
 
 // 비구조화 할당을 통하여 process.env 내부 값에 대한 레퍼런스 만들기
 const { PORT, MONGO_URI } = process.env;
@@ -25,6 +27,7 @@ mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
   .then(() => {
     console.log('Connected to MongoDB');
+    //createFakeData();
   })
   .catch(e => {
     console.error(e);
